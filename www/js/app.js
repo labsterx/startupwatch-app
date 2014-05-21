569449f2279bff4052b3c9b53acc97d9ac4660ed
labsterx <labsterx@gmail.com>
2014-05-22 00:16:23 +0800

    more inapp browser test

--- www/js/app.js
+++ www/js/app.js
@@ -62,5 +62,24 @@ angular.module('myApp', [
 
     $urlRouterProvider.otherwise('/app/search');
 
+})
+
+.directive('browseTo', function ($ionicGesture) {
+ return {
+  restrict: 'A',
+  link: function ($scope, $element, $attrs) {
+   var handleTap = function (e) {
+    // todo: capture Google Analytics here
+    var inAppBrowser = window.open(encodeURI($attrs.browseTo), '_system');
+   };
+   var tapGesture = $ionicGesture.on('tap', handleTap, $element);
+   $scope.$on('$destroy', function () {
+    // Clean up - unbind drag gesture handler
+    $ionicGesture.off(tapGesture, 'tap', handleTap);
+   });
+  }
+ }
 });
+
+
 
